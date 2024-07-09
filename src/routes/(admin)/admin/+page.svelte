2733d85ef309d4b482a24715257b549ac92c6cf3
Simone <xsimone97@hotmail.it>
2024-07-09 22:51:55 +0200

    placeholder backend table

--- src/routes/(admin)/admin/+page.svelte
+++ src/routes/(admin)/admin/+page.svelte
@@ -1,3 +1,32 @@
-<h1>
-    Admin area
-</h1>
+<script lang="ts">
+	import { onMount } from 'svelte';
+
+	onMount(async () => {});
+</script>
+
+<table class="table-auto">
+	<thead>
+		<tr>
+			<th>Song</th>
+			<th>Artist</th>
+			<th>Year</th>
+		</tr>
+	</thead>
+	<tbody>
+		<tr>
+			<td>The Sliding Mr. Bones (Next Stop, Pottersville)</td>
+			<td>Malcolm Lockyer</td>
+			<td>1961</td>
+		</tr>
+		<tr>
+			<td>Witchy Woman</td>
+			<td>The Eagles</td>
+			<td>1972</td>
+		</tr>
+		<tr>
+			<td>Shining Star</td>
+			<td>Earth, Wind, and Fire</td>
+			<td>1975</td>
+		</tr>
+	</tbody>
+</table>
