ffd8c41609de24510a35d7db375190a19be5d202
WinCisky <xsimone97@hotmail.it>
2024-07-14 16:47:18 +0200

    more details

--- src/routes/(admin)/admin/order/+page.svelte
+++ src/routes/(admin)/admin/order/+page.svelte
@@ -27,11 +27,70 @@
 							<div>
 								<div class="px-4 sm:px-0">
 									<h3 class="text-base font-semibold leading-7 text-gray-900">
+										Purchased product
+									</h3>
+								</div>
+								<div class="mt-6 border-t border-gray-100">
+									<dl class="divide-y divide-gray-100">
+										<div class="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
+											<dt class="text-sm font-medium leading-6 text-gray-900">Identifier</dt>
+											<dd class="mt-1 text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0">
+												{ order?.cart[0].id }
+											</dd>
+										</div>
+										<div class="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
+											<dt class="text-sm font-medium leading-6 text-gray-900">Variant</dt>
+											<dd class="mt-1 text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0">
+												{ order?.cart[0].proprieties.size }
+											</dd>
+										</div>
+									</dl>
+								</div>
+								<hr>
+								<div class="px-4 sm:px-0 mt-8">
+									<h3 class="text-base font-semibold leading-7 text-gray-900">
+										Shipping informations
+									</h3>
+								</div>
+								<div class="mt-6 border-t border-gray-100">
+									<dl class="divide-y divide-gray-100">
+										<div class="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
+											<dt class="text-sm font-medium leading-6 text-gray-900">Name</dt>
+											<dd class="mt-1 text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0">
+												{ order?.capture.purchase_units[0].shipping.name.full_name }
+											</dd>
+										</div>
+									</dl>
+									<dl class="divide-y divide-gray-100">
+										<div class="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
+											<dt class="text-sm font-medium leading-6 text-gray-900">Address</dt>
+											<dd class="mt-1 text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0">
+												{ `${order?.capture.purchase_units[0].shipping.address.address_line_1}, ${order?.capture.purchase_units[0].shipping.address.admin_area_1}, ${order?.capture.purchase_units[0].shipping.address.admin_area_2}` }
+											</dd>
+										</div>
+									</dl>
+									<dl class="divide-y divide-gray-100">
+										<div class="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
+											<dt class="text-sm font-medium leading-6 text-gray-900">Country code</dt>
+											<dd class="mt-1 text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0">
+												{ order?.capture.purchase_units[0].shipping.address.country_code }
+											</dd>
+										</div>
+									</dl>
+									<dl class="divide-y divide-gray-100">
+										<div class="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
+											<dt class="text-sm font-medium leading-6 text-gray-900">Postal code</dt>
+											<dd class="mt-1 text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0">
+												{ order?.capture.purchase_units[0].shipping.address.postal_code }
+											</dd>
+										</div>
+									</dl>
+								</div>
+								<hr>
+								<div class="px-4 sm:px-0 mt-8">
+									<h3 class="text-base font-semibold leading-7 text-gray-900">
 										Payment informations
 									</h3>
-									<p class="mt-1 max-w-2xl text-sm leading-6 text-gray-500">
-										Personal details
-									</p>
 								</div>
 								<div class="mt-6 border-t border-gray-100">
 									<dl class="divide-y divide-gray-100">
